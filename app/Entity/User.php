9e396318e246183fc588aac70817bbe38f1f4e98
yassuke <thiagom.devsec@gmail.com>
2021-01-16 10:35:50 -0300

    implements doctrine orm e configure .env :zap:

---
+++ app/Entity/User.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+/**
+ *  User Indetity
+ * 
+ * @Entity
+ * @Table(name="user")
+ */
+class User
+{
+    /**
+     * Primary key and user identifier
+     * 
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer",name="id")
+     */
+    private $_id;
+
+    /**
+     * @Column(type="string",name="email")
+     */
+    private $_email;
+
+    /**
+     * @Column(type="string",name="password")
+     */
+    private $_password;
+
+
+    public function getId(): int
+    {
+        return $this->_id;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->_email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->_email = $email;
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->_password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->_password = $password;
+        return $this;
+    }
+}
