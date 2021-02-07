456adfaa59f3b55de3133df54e904fb7a884a001
yassuke :crossed_swords <thiagom.devsec@gmail.com>
2021-02-07 13:27:56 -0300

    :recycle: change database driver and refactoring some stuffs

--- database/migrations/Version20210207162307.php
+++ database/migrations/Version20210207162307.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210123172835 extends AbstractMigration
+final class Version20210207162307 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,7 +20,7 @@ final class Version20210123172835 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL)');
     }
 
     public function down(Schema $schema) : void
