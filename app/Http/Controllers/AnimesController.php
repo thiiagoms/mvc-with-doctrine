3a210c3ccbb4bec57c4d9fea929a0c3096f3a00f
yassuke <thiagom.devsec@gmail.com>
2021-02-21 16:55:18 -0300

    fix animes interface :fire:

--- app/Http/Controllers/AnimesController.php
+++ app/Http/Controllers/AnimesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Interface\AnimesInterface;
+use App\Interfaces\AnimesInterface;
 use App\Entity\Anime;
 use Core\Infra\EntityManagerFactory;
 
--- app/Interfaces/AnimesInterface.php
+++ app/Interfaces/AnimesInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Interface;
+namespace App\Interfaces;
 
 interface AnimesInterface
 {
